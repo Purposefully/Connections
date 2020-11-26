37db5db38a60a56fce5dd6279c812730dcb1b598
Purposefully <grdnmathangl@aim.com>
2020-11-26 17:46:26 -0600

    started login/reg

--- login/models.py
+++ login/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import re
 
-# Create your models here.
+class UserManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['name']) < 2:
+            errors["name"] = "Name must be at least 2 characters long."
+        if len(postData['password']) < 8:
+            errors["password"] = "Password must be at least 8 characters long."
+        if postData['password'] != postData['confirm_password']:
+            errors["pwd_match"] = "Password must match Confirm PW"
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = "Email address is invalid."
+        return errors
+
+
+class User(models.Model):
+    name = models.CharField(max_length=45)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
--- login/views.py
+++ login/views.py
@@ -1,4 +1,67 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import User, UserManager
+import bcrypt
 
 def login(request):
-    pass
+    if request.method == "POST":
+        user = User.objects.filter(email=request.POST['email'])
+        if user: 
+            logged_user = user[0]
+            if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
+                request.session['user_id'] = logged_user.id
+                # For future reference, we had to add the .id to the line above to make it work
+                return redirect('/success')
+            else:
+                messages.error(request, "Incorrect password")
+                return redirect('/')
+        else:
+            messages.error(request, "Email not found")
+            return redirect('/')
+    return render(request, 'login.html')
+
+def signup(request):
+    if request.method == "POST":
+
+        errors = User.objects.basic_validator(request.POST)
+
+        # Check to make sure email is not already in db
+        duplicate = User.objects.filter(email=request.POST['email'])
+        if duplicate:
+            errors['registered'] = "A user account already exists with that email."
+
+        if errors:
+            for k, v in errors.items():
+                messages.error(request, v)
+            request.session['first_name'] = request.POST['first_name']
+            request.session['last_name'] = request.POST['last_name']
+            request.session['email'] = request.POST['email']
+            request.session['birthday'] = request.POST['birthday']
+            request.session['editing'] = True
+            return redirect('/')
+
+        #if no errors, add user to database
+        pwdhash = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+        User.objects.create(
+            first_name = request.POST['first_name'],
+            last_name = request.POST['last_name'],
+            email = request.POST['email'],
+            birthday = request.POST['birthday'],
+            password = pwdhash
+        )
+        # Remove entries from screen
+        request.session.flush()
+        request.session['user_id'] = User.objects.last().id
+        return redirect('/success')
+    return redirect('/')
+
+def success(request):
+    if 'user_id' in request.session:
+        return render(request, 'student_dashboard.html')
+    else:
+        return redirect('/')
+    #The order of this if-else mattered...
+
+def logout(request):
+    request.session.flush()
+    return redirect('/')
