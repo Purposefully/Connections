93f3970ba25dc2e53ed5372525844d68192a8735
Purposefully <grdnmathangl@aim.com>
2020-11-28 15:24:44 -0600

    finished Lesson Set Up page

--- lessons/views.py
+++ lessons/views.py
@@ -17,6 +17,25 @@ def teacher_dashboard(request):
 
 def teacher_choice(request):
     if 'user_id' in request.session:
-        return redirect(request, 'notice_solo.html')
+
+        # render new lesson screen
+        if request.GET['teacher_options'] == "new_lesson":
+            return render(request, 'new_lesson.html')
+
+        # redirect to view student work on a lesson
+        elif request.GET['teacher_options'] == "student_work":
+            return redirect(request, f'lesson/{request.GET.lesson_id}.html')
+
+        # redirect to display lesson code for students
+        elif request.GET['teacher_options'] == "post_code":
+            return redirect(request, f'lesson/{request.GET.lesson_id}.html')
+
+
     else:
         return redirect('/login/')
+
+# def new_lesson(request):
+#     if 'user_id' in request.session:
+#         if request.method == "POST":
+#             pass
+#         return render(request, 'new_lesson.html')
