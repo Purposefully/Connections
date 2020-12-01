1c8c2a83f08828caafc34399f98dc31c622818f9
Purposefully <grdnmathangl@aim.com>
2020-12-01 16:18:44 -0600

    updates

--- lessons/views.py
+++ lessons/views.py
@@ -18,18 +18,24 @@ def teacher_dashboard(request):
 def teacher_choice(request):
     if 'user_id' in request.session:
 
-        # render new lesson screen
-        if request.GET['teacher_options'] == "new_lesson":
+        # if user landed here via teacher dashboard form:
+        if request.method == "POST":
+
+            # render new lesson screen
+            if request.POST['teacher_options'] == "new_lesson":
+                return render(request, 'new_lesson.html')
+
+            # redirect to view student work on a lesson
+            elif request.POST['teacher_options'] == "student_work":
+                return redirect(request, f'lesson/{request.POST.lesson_id}.html')
+
+            # redirect to display lesson code for students
+            elif request.POST['teacher_options'] == "post_code":
+                return redirect(request, f'lesson/{request.POST.lesson_id}.html')
+
+        # if user clicked "previous screen" on new lesson page:
+        else:
             return render(request, 'new_lesson.html')
-
-        # redirect to view student work on a lesson
-        elif request.GET['teacher_options'] == "student_work":
-            return redirect(request, f'lesson/{request.GET.lesson_id}.html')
-
-        # redirect to display lesson code for students
-        elif request.GET['teacher_options'] == "post_code":
-            return redirect(request, f'lesson/{request.GET.lesson_id}.html')
-
 
     else:
         return redirect('/login/')
